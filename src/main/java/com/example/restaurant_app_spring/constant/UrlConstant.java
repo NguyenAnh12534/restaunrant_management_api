a7101f90ba7f65291ff9f1765af05859de2b15fa
NguyenAnh <Anh.Nguyen@iconplc.com>
2023-11-10 11:32:59 +0700

    Add Javadoc for UrlConstant class

--- src/main/java/com/example/restaurant_app_spring/constant/UrlConstant.java
+++ src/main/java/com/example/restaurant_app_spring/constant/UrlConstant.java
@@ -1,5 +1,8 @@
 package com.example.restaurant_app_spring.constant;
 
+/**
+ * This class contains constant for API URL
+ */
 public class UrlConstant {
     public static final String API_PREFIX = "/api";
     public static final String MENU_ITEM_ENDPOINT = API_PREFIX + "/menu-items";
